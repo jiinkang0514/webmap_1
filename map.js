48620543b18910e0ae2993cbbb88ac4d4d6b14a5
jiinkang0514 <crunkgraph@gmail.com>
2025-05-19 18:20:28 +0900

    Updated map.js with access token

--- map.js
+++ map.js
@@ -1,1 +1,10 @@
 console.log("Hello, world!")
+
+'use strict'        // let the browser know we're serious
+
+// debug statement letting us know the file is loaded
+console.log('Loaded map.js')
+
+// your mapbox token
+mapboxgl.accessToken = 'pk.eyJ1IjoiamlpbmthbmdnIiwiYSI6ImNtYXV1MDh1aDAwdnoya3B5NnFlcWh5NmYifQ.ZXjsL-aQyFYxyG5p5XI_lA'
+
