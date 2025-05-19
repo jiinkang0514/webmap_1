3512f77a9265d6e2895d073b2926859a9e272cfa
jiinkang0514 <crunkgraph@gmail.com>
2025-05-19 18:33:50 +0900

    Update: latest changes to map files

--- map.js
+++ map.js
@@ -1,5 +1,3 @@
-console.log("Hello, world!")
-
 'use strict'        // let the browser know we're serious
 
 // debug statement letting us know the file is loaded
@@ -8,3 +6,9 @@ console.log('Loaded map.js')
 // your mapbox token
 mapboxgl.accessToken = 'pk.eyJ1IjoiamlpbmthbmdnIiwiYSI6ImNtYXV1MDh1aDAwdnoya3B5NnFlcWh5NmYifQ.ZXjsL-aQyFYxyG5p5XI_lA'
 
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/light-v9',
+    center: [-73.93324, 40.80877],
+    zoom: 14
+});
