d80fcd6060da25c4165b773dda969098edf32bcb
jiinkang0514 <jiinkang20@gmail.com>
2025-06-07 21:41:39 +0900

    Update map.js and add geojson data

--- map.js
+++ map.js
@@ -12,3 +12,111 @@ var map = new mapboxgl.Map({
     center: [-73.93324, 40.80877],
     zoom: 14
 });
+
+var blocks_url = "./data/blocks_joined_trees_um.geojson"
+
+map.on('load',function(){
+  // define a 'source' for your polygons dataset
+  map.addSource('blocks_data',{
+    'type':'geojson',
+    'data': blocks_url,
+  });
+  // add a new layer with your polygons
+  map.addLayer({
+    'id':'blocks',
+    'type':'fill',
+    'source':'blocks_data',
+    'paint':{
+      'fill-color':'#ffffff',
+      'fill-outline-color':'#000000',
+      'fill-opacity': 0.5
+    }
+  })
+});
+
+var trees_url = "./data/2015_Street_Tree_Census_subset_um.geojson"
+
+map.addSource('trees_data',{
+      'type':'geojson',
+      'data': trees_url
+    });
+    // add a new layer with your points
+    map.addLayer({
+      'id':'trees',
+      'type':'circle',
+      'source':'trees_data',
+      'paint':{
+        'circle-color': '#349f27',
+        'circle-opacity':0.7,
+        'circle-radius':4
+      },
+    })
+
+map.addLayer({
+    'id':'blocks',
+    'type':'fill',
+    'source':'blocks_data',
+    'paint':{
+        'fill-color': 
+          ['case', 
+          ['==', ['get', 'avg_diamet'], null],
+          'white',
+          ['step', ['get', 'avg_diamet'],
+            '#ffffff',
+            2.615, '#edf8e9',
+            6.444, '#bae4b3',
+            9.379, '#74c476',
+            15.036, '#31a354',
+            26.000, '#006d2c'
+          ]],
+      'fill-outline-color':'#000000',
+      'fill-opacity': 0.5
+    }
+  })
+
+  map.addSource('trees_data',{
+      'type':'geojson',
+      'data': trees_url
+    });
+    // add a new layer with your points
+    map.addLayer({
+      'id':'trees',
+      'type':'circle',
+      'source':'trees_data',
+      'paint':{
+        'circle-color': '#349f27',
+        'circle-opacity':0.7,
+        'circle-radius': ['/', ['get', 'tree_dbh'], 5],
+      },
+    })
+
+// listen for 'onclick' events from 'handler-element'
+document.getElementById('handler-element').onclick = handleEvent;
+
+// say how to handle the events that are fired by the listener
+function handleEvent(e) {
+  console.log("an event happened")
+}
+
+map.on('click', 'trees', function(e) {
+  // get the map coordinates of the feature
+  var coordinates = e.features[0].geometry.coordinates.slice();
+  // get its species name from the feature's attributes
+  var species = e.features[0].properties.spc_common;
+
+  // and create a popup on the map
+  new mapboxgl.Popup()
+  .setLngLat(coordinates)
+  .setHTML(species)
+  .addTo(map);
+});
+  
+// make the cursor a pointer when over the tree
+map.on('mouseenter', 'trees', function() {
+  map.getCanvas().style.cursor = 'pointer';
+});
+  
+// back to normal when it's not
+map.on('mouseleave', 'trees', function() {
+  map.getCanvas().style.cursor = '';
+});
